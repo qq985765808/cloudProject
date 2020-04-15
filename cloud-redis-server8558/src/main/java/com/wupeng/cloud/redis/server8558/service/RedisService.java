59e4c5352bca80fbad0fe7d767e86af7b572d4eb
kingpeng <2724418180@qq.com>
2020-04-15 09:10:40 +0800

    add  support redis by list 建议先从序列化了解

--- cloud-redis-server8558/src/main/java/com/wupeng/cloud/redis/server8558/service/RedisService.java
+++ cloud-redis-server8558/src/main/java/com/wupeng/cloud/redis/server8558/service/RedisService.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -24,6 +25,15 @@ public class RedisService {
     @Resource
     private RedisTemplate<String,Object> redisTemplate;
 
+    //********************************   String ******************************//
+    /**
+     * 设置key和value
+     * @author Administrator
+     * @date 2020-04-15
+     * @param key
+     * @param value
+     * @return
+     */
     public void set(String key, Object value) {
         //更改在redis里面查看key编码问题
         RedisSerializer redisSerializer =new StringRedisSerializer();
@@ -34,15 +44,18 @@ public class RedisService {
 
 
 
+
     /**
      * 设置过期时间<br>
-     *  timeout:设置多少秒,unit是时间单位（年月日时分秒，具体可看此类）
+     * timeout:设置多少秒,unit是时间单位（年月日时分秒，具体可看此类）
+     * @author Administrator
+     * @date 2020-04-15
      * @param key
      * @param value
      * @param timeout
      * @param unit
-     *
-     * */
+     * @return
+     */
     public void set(String key, Object value, Long timeout, TimeUnit unit){
         //更改在redis里面查看key编码问题
         RedisSerializer redisSerializer =new StringRedisSerializer();
@@ -64,13 +77,18 @@ public class RedisService {
     }
 
     /**
-     *  获取key键对应的值
-     * */
+     * 获取key的长度
+     * @author Administrator
+     * @date 2020-04-15
+     * @param key
+     * @return
+     */
     public Long getSize(String key){
         return  redisTemplate.opsForValue().size(key);
     }
 
 
+    //********************************   hash ******************************//
     /**
      * 新增hashMap值
      * */
@@ -168,6 +186,7 @@ public class RedisService {
         return redisTemplate.expire(key,timeout,unit);
     }
 
+    //********************************   zset ******************************//
     /**
      *添加一个元素, zset与set最大的区别就是每个元素都有一个score，因此有个排序的辅助功能;  zadd
      * @author Administrator
@@ -296,5 +315,93 @@ public class RedisService {
         return redisTemplate.opsForZSet().rangeByScore(key, min, max);
     }
 
+    //********************************   list ******************************//
+    /**
+     * 在列表的最左边塞入一个value,取出来的话建议用leftPoP遍历
+     * @author Administrator
+     * @date 2020-04-15 
+     * @param key
+     * @param value
+     * @return
+     */
+    public void leftPush(String key, String value) {
+        redisTemplate.opsForList().leftPush(key, value);
+    }
+
+    /**
+     * 在列表的最右边塞入一个value,取出来的话建议用rightPop遍历
+     * @author Administrator
+     * @date 2020-04-15
+     * @param key
+     * @param value
+     * @return
+     */
+    public void rightPush(String key, String value) {
+        redisTemplate.opsForList().leftPush(key, value);
+    }
+
+    /**
+     * 获取指定索引位置的值, index为-1时，表示返回的是最后一个；当index大于实际的列表长度时，返回null
+     * @author Administrator
+     * @date 2020-04-15 
+     * @param key
+     * @param index
+     * @return
+     */
+    public Object lindex(String key, int index) {
+        return redisTemplate.opsForList().index(key, index);
+    }
+
+    /**
+     * 获取范围值，闭区间，start和end这两个下标的值都会返回; end为-1时，表示获取的是最后一个；
+     * @author Administrator
+     * @date 2020-04-15 
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public List lrange(String key, int start, int end) {
+        return redisTemplate.opsForList().range(key, start, end);
+    }
+
+    /**
+     * 设置list中指定下标的值，采用干的是替换规则, 最左边的下标为0；-1表示最右边的一个
+     * @author Administrator
+     * @date 2020-04-15 
+     * @param key
+     * @param index
+     * @param value
+     * @return
+     */
+    public void set(String key, Integer index, String value) {
+        redisTemplate.opsForList().set(key, index, value);
+    }
+
+    /**
+     * 删除列表中值为value的元素，总共删除count次
+     * @author Administrator
+     * @date 2020-04-15
+     * @param key
+     * @param value
+     * @param count
+     * @return
+     */
+    public void lremove(String key, String value, int count) {
+        redisTemplate.opsForList().remove(key, count, value);
+    }
+
+    /**
+     * 删除list首尾，只保留 [start, end] 之间的值
+     * @author Administrator
+     * @date 2020-04-15 
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    public void trim(String key, Integer start, Integer end) {
+        redisTemplate.opsForList().trim(key, start, end);
+    }
 
 }
